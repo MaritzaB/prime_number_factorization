b20d3f6096827446e67d83aaadcb22611839ffcf
maritzee <amby.121192@gmail.com>
2022-05-28 14:23:47 -0700

    ✨ Add anayzer function 🧐 📈

---
+++ src/analizer.py
@@ -0,0 +1,14 @@
+from prime_number_fact import criba, number, euclides, euclidex, euclidex1
+
+first_number = 18
+second_number = 12
+print("El máximo común divisor entre 18 y 12",euclidex(first_number,second_number))
+print("El máximo común divisor entre 18 y 12",euclidex1(first_number,second_number))
+
+#n=100
+#p = criba(n)  # p lo usa facp
+#print("Criba de {n} \n", criba(n),"\n")
+
+#print("Encontrar coordenadas primas", facp(n),"\n")
+
+#print(number(criba(n)),"\n")
