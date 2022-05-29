9eef134a0082654201095812b2240b25ad691c03
maritzee <amby.121192@gmail.com>
2022-05-29 00:50:39 -0700

    ⚡️ Improve analizer

--- src/analizer.py
+++ src/analizer.py
@@ -1,46 +1,45 @@
-from prime_number_fact import criba, number, euclides, euclidex, euclidex1, phi
+from prime_number_fact import calculatePrimeFactors
+from N17_euclidex1 import euclidex1, euclidex
 import time
-from random import getrandbits
+from random import getrandbits, seed
 import numpy as np
 
-# Generar lista de números aleatorios basados en bits
-lista = []
-for i in [2,5,10,15,20,25,30,35,40]:
-    lista.append(getrandbits(i))
 
-print(lista)
+def listOfRandbits():
+    seed(5)
+    randbits_list = []
+    for i in [3, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55]:
+        randbits_list.append(getrandbits(i))
+    return randbits_list
 
-first_number = 15
-second_number = 27
-numbers = (first_number, second_number)
-n=48
-#print(f'phi de {n} es ',phi(n))
-#print(f'Criba de {n} es', criba(n))
-#
-print(euclidex(first_number, second_number))
-print(euclidex1(first_number, second_number))
-#print(euclides(first_number, second_number))
 
-def timer(function, numbers):
-    init_time = time.time_ns()
-    function(numbers[0], numbers[1])
-    final_time = time.time_ns()
+def timer(function, number):
+    init_time = time.time()
+    function(number)
+    final_time = time.time()
     total_time = final_time - init_time
     return total_time
 
 
+rand_num_list = listOfRandbits()
+# timer_list = []
+
+for i in range(len(rand_num_list)):
+    print(f"Número aleatorio: ", rand_num_list[i])
+    # timer_list.append(timer(calculatePrimeFactors,rand_num_list[i]))
+    print(f"Tiempo de cálculo: ", timer(calculatePrimeFactors, rand_num_list[i]))
+
+
 run_times = 15
-time_euclidex = []
-time_euclidex1 = []
-time_euclides = []
+time_1st_algorithm = []
 
-for r in range(run_times):
-    time_euclidex.append(timer(euclidex, numbers))
-    time_euclidex1.append(timer(euclidex1, numbers))
-    time_euclides.append(timer(euclides, numbers))
-    # print(f"Run number {r} for {euclidex.__name__}",timer(euclidex, numbers))
-    # print(f"Run number {r} for {euclidex1.__name__}",timer(euclidex1, numbers))
-    # print(f"Run number {r} for {euclides.__name__}",timer(euclides, numbers))
-    # print("-----\n")
 
-#print(time_euclidex, "\n", time_euclidex1, "\n", time_euclides)
+# for r in range(run_times):
+#    time_1st_algorithm.append(timer(euclidex, numbers))
+
+# print(f"Run number {r} for {euclidex.__name__}",timer(euclidex, numbers))
+# print(f"Run number {r} for {euclidex1.__name__}",timer(euclidex1, numbers))
+# print(f"Run number {r} for {euclides.__name__}",timer(euclides, numbers))
+# print("-----\n")
+
+# print(time_euclidex, "\n", time_euclidex1, "\n", time_euclides)
