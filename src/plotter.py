11745106c8cca8cb48d950b4a1034f9dc6bc2815
maritzee <amby.121192@gmail.com>
2022-05-30 16:52:47 -0700

    ✨💥 Fix plotter

--- src/plotter.py
+++ src/plotter.py
@@ -1,44 +1,48 @@
-from analizer import timeScorer
+from analizer import timeScorer, max_bits
 from pathlib import Path
 from prime_number_fact import sequentialFactorization, trialDivision
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
-filepath = Path("reports/table/out.csv")
-
-
-list1 = np.array(timeScorer(sequentialFactorization))
-list2 = np.array(timeScorer(trialDivision))
-
-time_score_table = [list1, list2]
+max_bits = 50
+# filepath = Path("reports/table/out.csv")
 
 
-def ratio(list1, list2):
-    for i in range(len(list1)):
-        if list1[i] < list2[i]:
-            print("gana lista1")
-        else:
-            print("gana lista2")
+seq_fact_list = np.array(timeScorer(sequentialFactorization))
+trial_div_list = np.array(timeScorer(trialDivision))
+
+time_score_table = [seq_fact_list, trial_div_list]
 
 
-ratio = ratio(list1, list2)
+def plotter(max_bits):
+    """Grafica complejidad practica"""
+    plt.xlabel("Tamaño de n (bits)")
+    plt.ylabel("Tiempo (segundos)")
+    plt.title("Complejidad práctica")
+    bits = [x for x in range(1, max_bits + 1)]
+    for nlist in time_score_table:
+        plt.plot(bits, nlist)
+        plt.savefig("practical_complexity.png")
+
+
+plotter(max_bits)
+
+
+# def ratio(seq_fact_list, trial_div_list):
+#    for i in range(len(seq_fact_list)):
+#        if seq_fact_list[i] < trial_div_list[i]:
+#            print("gana lista1")
+#        else:
+#            print("gana lista2")
+
+
+# ratio = ratio(seq_fact_list, trial_div_list)
 
 # time_score_table.append(ratio)
 
-lambdas_table = pd.DataFrame(time_score_table)  # .pivot(columns='Date', values='Value')
+# lambdas_table = pd.DataFrame(time_score_table)  # .pivot(columns='Date', values='Value')
 
 # lambdas_table.columns = ["Islet", "Growth_rate", "p_value", "p_value_menor"]
 
-lambdas_table.to_csv(filepath, index=False)
-
-# Plot practical complexity
-# plt.xlabel('Bits')
-# plt.ylabel('Tiempo (segundos)')
-# plt.title('Complejidad práctica')
-# bits = [x for x in range(1,max_bits)]
-#
-# plt.plot(bits,timer_list_Prime_Factors)
-# plt.plot(bits,timer_list_Prime_trialDivision)
-# plt.savefig("practical_complexity.png")
-#
-# Clear the figure
+# lambdas_table.to_csv(filepath, index=False)
