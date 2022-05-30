5ba0846f262a66a9b92cb928591cd28af556d97b
maritzee <amby.121192@gmail.com>
2022-05-30 16:52:05 -0700

    📝 Update documentation

--- src/analizer.py
+++ src/analizer.py
@@ -5,18 +5,23 @@ import numpy as np
 import pandas as pd
 import time
 
+
 max_bits = 50
 
 
 def randBits(max_bits):
+    """Genera una lista de numeros enteros aleatorios
+    cuyos elementos aumentan en el tamano de las cifras"""
     seed(68)
     randbits_list = []
-    for i in range(1, max_bits):
+    for i in range(1, max_bits + 1):
         randbits_list.append(getrandbits(i))
     return randbits_list
 
 
 def timer(function, number):
+    """Toma el tiempo de ejecucion de un
+    algoritmo para un numero dado"""
     init_time = time.time()
     function(number)
     final_time = time.time()
@@ -25,6 +30,8 @@ def timer(function, number):
 
 
 def timeScorer(factFunction):
+    """Toma el tiempo de ejecucion de un
+    algoritmo para una lista de numeros dados"""
     rand_num_list = randBits(max_bits)
     time_score = []
     for i in range(len(rand_num_list)):
