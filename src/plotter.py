d191095feb464b22ce81da33bdd7dd835269dcb6
maritzee <amby.121192@gmail.com>
2022-05-30 19:31:06 -0700

    🏆 Plot winner

--- src/plotter.py
+++ src/plotter.py
@@ -6,43 +6,57 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 max_bits = 50
-# filepath = Path("reports/table/out.csv")
+
+
+def plotter(max_bits, comparative_table):
+    """Grafica complejidad practica"""
+    plt.xlabel("Tamano de n (bits)")
+    plt.ylabel("Tiempo (segundos)")
+    plt.title("Complejidad practica")
+    plt.xscale("log")
+    plt.yscale("log")
+    bits = [x for x in range(1, max_bits + 1)]
+    plt.plot(bits, comparative_table[0], label = "Factorizacion secuencial")
+    plt.plot(bits, comparative_table[1], label = "Division tentativa")
+    plt.legend()
+    plt.savefig("reports/figures/practical_complexity.png")
+
+
+def pickWinner(seq_fact_list, trial_div_list):
+    winner_table = []
+    for i in range(len(seq_fact_list)):
+        if seq_fact_list[i] < trial_div_list[i]:
+            winner_table.append("Factorizacion secuencial")
+        else:
+            winner_table.append("Division tentativa")
+    winner_table = np.array(winner_table)
+    return winner_table
 
 
 seq_fact_list = np.array(timeScorer(sequentialFactorization))
 trial_div_list = np.array(timeScorer(trialDivision))
 
-time_score_table = [seq_fact_list, trial_div_list]
+# Calculate ratio between algorithms
+ratio = np.array(trial_div_list / seq_fact_list)
 
+# Pick winner
+winner = np.array(pickWinner(seq_fact_list, trial_div_list))
 
-def plotter(max_bits):
-    """Grafica complejidad practica"""
-    plt.xlabel("Tamaño de n (bits)")
-    plt.ylabel("Tiempo (segundos)")
-    plt.title("Complejidad práctica")
-    bits = [x for x in range(1, max_bits + 1)]
-    for nlist in time_score_table:
-        plt.plot(bits, nlist)
-        plt.savefig("practical_complexity.png")
-
-
-plotter(max_bits)
-
-
-# def ratio(seq_fact_list, trial_div_list):
-#    for i in range(len(seq_fact_list)):
-#        if seq_fact_list[i] < trial_div_list[i]:
-#            print("gana lista1")
-#        else:
-#            print("gana lista2")
-
-
-# ratio = ratio(seq_fact_list, trial_div_list)
-
+# Fill table
+time_score_table = []
 # time_score_table.append(ratio)
+time_score_table.append(seq_fact_list)
+time_score_table.append(trial_div_list)
+# time_score_table.append(winner)
 
-# lambdas_table = pd.DataFrame(time_score_table)  # .pivot(columns='Date', values='Value')
+time_score_table = np.array(time_score_table)
 
-# lambdas_table.columns = ["Islet", "Growth_rate", "p_value", "p_value_menor"]
+# pd.DataFrame(time_score_table).to_csv("file.csv", index=False)
 
-# lambdas_table.to_csv(filepath, index=False)
+# time_score_table = pd.DataFrame(time_score_table)
+
+
+# Plot complexity
+plotter(max_bits, time_score_table)
+
+# time_score_table.to_csv(filepath, index=False)
